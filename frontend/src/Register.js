4420ee1b01307a323eba5e69dfb79a141ba89c12
Michael <mikophg2001@gmail.com>
2023-01-10 18:33:15 -0800

    finished register user into database

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios';
+
+const registerUrl = 'https://noc4wfai1i.execute-api.us-east-1.amazonaws.com/prod/register'; // Should make as environment variable
 
 const Register = () => {
-    return (
-        <div>
-            This is the register page!
-        </div>
-    )
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const [message, setMessage] = useState(null);
+
+	const submitHandler = (event) => {
+	    event.preventDefault();
+	    if (username.trim() === '' || email.trim() === '' || name.trim() === '' || password.trim() === '') {
+			setMessage('All fields are required');
+			return;
+	    }
+	    
+		const requestConfig = {
+			headers: {
+				'x-api-key': 'HcBRcNghJa3LwKQHT9GK91O9Fe3McmEy6mdNt4IU' // Should make as environment variable
+			}
+		}
+		const requestBody = {
+			username: username,
+			email: email,
+			name: name,
+			password: password
+		}
+		axios.post(registerUrl, requestBody, requestConfig).then(response => {
+			setMessage('Registration Successful');
+		}).catch(error => {
+			if (error.response.status === 401 || error.response.status === 403) {
+				setMessage(error.response.data.message);
+			}
+			else {
+				setMessage('Sorry. Backend server is down. Please try again later.');
+			}
+		})
+
+	}
+
+	return (
+		<div>
+			<form onSubmit={submitHandler}>
+				<h5>Register</h5>
+				name: <input type="text" value={name} onChange={event => setName(event.target.value)} /> <br/>
+				email: <input type="text" value={email} onChange={event => setEmail(event.target.value)} /> <br/>
+				username: <input type="text" value={username} onChange={event => setUsername(event.target.value)} /> <br/>
+				password: <input type="password" value={password} onChange={event => setPassword(event.target.value)} /> <br/>
+				<input type="submit" value="Register" />
+			</form>
+			{message && <p className="message">{message}</p>}
+		</div>
+	)
 }
 
 export default Register;
